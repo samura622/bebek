699d0be2cb0e54be09f265d85f879d1aaf6a49a5
samura622 <97351353+samura622@users.noreply.github.com>
2023-10-29 20:54:52 -0700

    Add files via upload

---
+++ DependencyPractice/src/main/java/com/example/App.java
@@ -0,0 +1,16 @@
+package com.example;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String text = "Hello Maven";
+        String reversed = StringUtils.reverse(text);
+        System.out.println(reversed);
+    }
+}
